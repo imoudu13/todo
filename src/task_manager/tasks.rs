1c0c79b3c35786d69ed975b1c7fbdf44c7daf94a
Imoudu Ibrahim <72046339+imoudu13@users.noreply.github.com>
2025-06-26 22:10:32 -0700

    added function that gets info for tasks and saves it

--- src/task_manager/tasks.rs
+++ src/task_manager/tasks.rs
@@ -1,2 +1,29 @@
-use tokio_postgres::{NoTls, Error};
+use crate::db_conn::db_manager::add_task;
+use std::io;
 
+pub async fn get_task_info() -> Option<u64> {
+    let mut title: String = String::new();
+    let mut description: String = String::new();
+
+    println!("Enter title: {}", title);
+    io::stdin()
+        .read_line(&mut title)
+        .expect("Failed to read line");
+
+    println!("Enter description: {}", title);
+    io::stdin()
+        .read_line(&mut description)
+        .expect("Failed to read line");
+    
+    println!("g");
+    match add_task(title.as_str(), description.as_str()).await {
+        Ok(rows_affected) => {
+            println!("Added {} rows.", rows_affected);
+            Some(rows_affected)
+        },
+        Err(error) => {
+            eprintln!("Failed to add task: {}", error);
+            None
+        }
+    }
+}
