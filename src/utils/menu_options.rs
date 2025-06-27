32d50b93c164ef16c33bd672d3d77d803691608e
Imoudu Ibrahim <72046339+imoudu13@users.noreply.github.com>
2025-06-26 18:11:41 -0700

    add utils module

---
+++ src/utils/menu_options.rs
@@ -0,0 +1,10 @@
+pub fn list_options() {
+    println!("
+=== MENU ===
+1. Add task
+2. List tasks
+3. Remove task
+4. Mark task done
+0. Exit
+Enter the number on the left of the menu to select an option:");
+}
